03b8f816b5f1b13a2d436438742533376c151e35
Tim-Speed <iceczd@gmail.com>
2013-05-18 14:25:46 -0300

    Should work, needs testing

--- SousChef.js
+++ SousChef.js
@@ -27,7 +27,8 @@ var SousChef = function() {
 
 					console.log("Got Job!");
 					
-					jobs[JobName] = JobScript;
+					// For now I'm just going to eval to build the functions
+					jobs[JobName] = eval("(" + JobScript + ")") ;
 					
 					break;
 				case "submitJob":
@@ -37,23 +38,38 @@ var SousChef = function() {
 
 					console.log("Job Available: " + JobName);
 					
-					// Request the job data
-					socket.emit("requestJobData", {
-						name: JobName
-					});
+					if (jobs[JobName]) {
+						// Request the job data
+						socket.emit("requestJobData", {
+							name: JobName
+						});
+					}
 					break;
 				case "requestJobData":
 					// Got job data
+					var JobName = message.name;
 					var id = message.id;
 					var data = message.data;
 					
 					console.log("Got job data! " + id);
 					
-					// Send data after job is done
-					socket.emit("jobComplete", {
-						id: id,
-						status: 0,
-						data: data
+					// Call the job with the specified data, and wait for it to call the callback
+					jobs[JobName](data, function(nextJob, data) {
+						// Build output to queue
+						var out = {
+							id: id,
+							status: 0
+						};
+						
+						// If we have data and job to pass it to specfied
+						if (data && nextJob) {
+							// Append to output json
+							out.next = nextJob;
+							out.data = data;
+						}
+						
+						// Send data after job is done
+						socket.emit("jobComplete", out);
 					});
 					break;
 			}
